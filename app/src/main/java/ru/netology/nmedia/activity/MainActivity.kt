17f8062ab025116970d4d1300d8f80cc841b4e27
Ivan Melnikov <johnny-mill@yandex.ru>
2022-12-25 17:41:09 +0300

    layout: use RecyclerView

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,9 +3,8 @@ package ru.netology.nmedia.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.helpers.HelperNumbers.compactNumber
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -15,27 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                likes.text = compactNumber(post.likes)
-                shares.text = compactNumber(post.shares)
-                views.text = compactNumber(post.views)
-                liked.setImageResource(
-                    if (post.liked) R.drawable.ic_baseline_favorite_24
-                    else R.drawable.ic_baseline_favorite_border_24
-                )
-            }
-        }
-
-        binding.liked.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.shared.setOnClickListener {
-            viewModel.share()
+        val adapter = PostsAdapter(
+            { viewModel.like(it.id) },
+            { viewModel.share(it.id) }
+        )
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.list = posts
         }
     }
 }
