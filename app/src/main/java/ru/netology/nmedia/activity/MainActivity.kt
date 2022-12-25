34d6420c7299089522059d84d166e69b2eead1cb
Ivan Melnikov <johnny-mill@yandex.ru>
2022-12-25 22:36:06 +0300

    app: replace Adapter by ListAdapter

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         )
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.list = posts
+            adapter.submitList(posts)
         }
     }
 }
