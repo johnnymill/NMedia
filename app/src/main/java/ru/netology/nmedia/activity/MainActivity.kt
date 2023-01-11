7441fbc25a99cb403d1639bccaedabbbd27c7b6b
Ivan Melnikov <johnny-mill@yandex.ru>
2023-01-11 22:54:38 +0300

    app: add dedicated activity for modifying posts

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -2,8 +2,6 @@ package ru.netology.nmedia.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
@@ -11,7 +9,6 @@ import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.helpers.HelperAndroid
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
+
+        val editPostLauncher = registerForActivityResult(EditPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            viewModel.save()
+        }
+
         val adapter = PostsAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
                 viewModel.edit(post)
+                editPostLauncher.launch(post.content)
             }
 
             override fun onRemove(post: Post) {
@@ -53,47 +58,8 @@ class MainActivity : AppCompatActivity() {
             adapter.submitList(posts)
         }
 
-        viewModel.edited.observe(this) { post ->
-            if (post.id == 0L) {
-                return@observe
-            }
-            with(binding.editedContent) {
-                requestFocus()
-                setText(post.content)
-            }
-            binding.groupCancel.visibility = View.VISIBLE
-        }
-
-        binding.cancel.setOnClickListener {
-            with(binding.editedContent) {
-                viewModel.cancel()
-
-                setText("")
-                clearFocus()
-                HelperAndroid.hideKeyboard(this)
-            }
-            binding.groupCancel.visibility = View.GONE
-        }
-
-        binding.save.setOnClickListener {
-            with(binding.editedContent) {
-                if (text.isNullOrBlank()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        context.getString(R.string.error_empty_content),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                viewModel.changeContent(text.toString())
-                viewModel.save()
-
-                setText("")
-                clearFocus()
-                HelperAndroid.hideKeyboard(this)
-            }
-            binding.groupCancel.visibility = View.GONE
+        binding.fab.setOnClickListener {
+            editPostLauncher.launch(null)
         }
     }
 }
