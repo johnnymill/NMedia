d66e8c38871d64fca436b36492b8ce3b9531be99
Ivan Melnikov <johnny-mill@yandex.ru>
2023-02-16 12:48:02 +0300

    edit post: introduce saving draft of added post

--- app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -1,14 +1,19 @@
 package ru.netology.nmedia.activity
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentEditPostBinding
 import ru.netology.nmedia.helpers.HelperAndroid
@@ -24,6 +29,11 @@ class EditPostFragment : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding: FragmentEditPostBinding
         get() = _binding!!
+
+    private lateinit var gson: Gson
+    private var prefs: SharedPreferences? = null
+    private val type = TypeToken.getParameterized(String::class.java).type
+    private val key = "content"
 
     private val viewModel: PostViewModel by viewModels(
         ownerProducer = ::requireParentFragment
@@ -44,7 +54,27 @@ class EditPostFragment : Fragment() {
 
         binding.edit.requestFocus()
 
-        arguments?.textArg?.let(binding.edit::setText)
+        val textArg = arguments?.textArg
+        if (!textArg.isNullOrBlank()) {
+            // We are in edit mode
+            binding.edit.setText(textArg)
+        } else {
+            // We are in add mode
+            gson = Gson()
+            prefs = requireContext().getSharedPreferences("draft", Context.MODE_PRIVATE)
+
+            prefs!!.getString(key, null)?.let {
+                binding.edit.text = gson.fromJson(it, type)
+            }
+            // This callback will only be called when EditPostFragment is at least Started.
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                with(prefs!!.edit()) {
+                    putString(key, gson.toJson(binding.edit.text.toString()))
+                    apply()
+                }
+                findNavController().navigateUp()
+            }
+        }
 
         binding.save.setOnClickListener {
             if (binding.edit.text.isNullOrBlank()) {
@@ -53,6 +83,13 @@ class EditPostFragment : Fragment() {
             } else {
                 viewModel.changeContent(binding.edit.text.toString())
                 viewModel.save()
+                if (prefs != null) {
+                    // Delete the draft (make it empty)
+                    with(prefs!!.edit()) {
+                        putString(key, gson.toJson(""))
+                        apply()
+                    }
+                }
                 HelperAndroid.hideKeyboard(requireView())
                 findNavController().navigateUp()
             }
